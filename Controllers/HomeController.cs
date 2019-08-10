9c575bd00c207558a5886b7b71be1148d7931b75
Ryan Baermann <rcbaermann@gmail.com>
2019-08-10 15:12:06 -0700

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace razorFun
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
